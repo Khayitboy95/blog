aa585b2f9d1e46f8465bd2fe61f0c4d09666defd
unknown <ergashevx33@gmail.com>
2021-07-15 22:43:18 +0500

    add user model

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const cardRoutes = require('./routes/card')
 const mongoose = require('mongoose')
+const User = require('./models/user')
 
 const app = express()
 
@@ -18,6 +19,16 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('60f061818c1a0f18cc36fecf');
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+})
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
@@ -31,6 +42,15 @@ const PORT = process.env.PORT || 3000
 const start = async () => {
   try {
     await mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    const candidate = await User.findOne();
+    if(!candidate) {
+      const user = new User({
+        email: 'ergashevx33@gmail.com',
+        name: 'Khayitboy',
+        cart: {items: []}
+      })
+      await user.save();
+    }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
