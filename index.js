10891ae18ab823c96bf7219e8721f14ce5d2f259
Эргашев Хайитбой Ходжирахим угли <93647@umk.uz>
2021-07-15 18:28:28 +0500

    working with mongodb

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const cardRoutes = require('./routes/card')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -27,6 +28,16 @@ app.use('/card', cardRoutes)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost/blog', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start();
+
